2a7ec75082bac218435a0ac3cef392e1577a8d72
Niki1999 <42090221+Niki1999@users.noreply.github.com>
2018-08-04 14:35:23 +0300

    FirstCommit

---
+++ Lecture1Introduction/Task1RectArea/Task1RectArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1RectArea
+{
+    class Task1RectArea
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
